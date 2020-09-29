94b79a965554b5d7e68033c8eeeb52a69f62d21c
Ali Madooei <madooei@jhu.edu>
2020-09-29 09:21:10 -0400

    Step 3

---
+++ script.js
@@ -0,0 +1,51 @@
+// sample schools
+schools = [
+  {
+    Name: "School of Medicine",
+  },
+  {
+    Name: "Whiting School of Engineering",
+  },
+];
+
+// sample terms
+terms = [
+  {
+    Name: "Fall 2020",
+  },
+  {
+    Name: "Summer 2020",
+  },
+  {
+    Name: "Spring 2020",
+  },
+];
+
+// sample courses
+// (SIS returns provides several other properties for a course)
+courses = [
+  {
+    OfferingName: "EN.601.226",
+    SectionName: "01",
+    Title: "Data Structures",
+    Instructors: "A. Madooei",
+  },
+  {
+    OfferingName: "EN.601.226",
+    SectionName: "02",
+    Title: "Data Structures",
+    Instructors: "A. Madooei",
+  },
+  {
+    OfferingName: "EN.601.280",
+    SectionName: "01",
+    Title: "Full-Stack JavaScript",
+    Instructors: "A. Madooei",
+  },
+  {
+    OfferingName: "EN.601.280",
+    SectionName: "02",
+    Title: "Full-Stack JavaScript",
+    Instructors: "A. Madooei",
+  },
+];
